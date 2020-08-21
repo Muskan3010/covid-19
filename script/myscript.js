0efa6d79a8fcf5e4574f35974e4e106dafa82a56
Muskan <muskanverma3010@gmail.com>
2020-08-22 01:15:59 +0530

    Next version of web file.

---
+++ script/myscript.js
@@ -0,0 +1,37 @@
+const URL="https://covid19.mathdro.id/api";
+//https://covid19.mathdro.id/api/countries/india
+let app= angular.module("MyApp", []);
+
+app.controller("MyCtrl", ($scope,$http)=>{
+  //this is controller
+  $scope.title="Stay Home Stay Safe";
+
+  console.log("app loaded");
+
+    //calling api
+  $http.get(URL).then((response)=>{
+      //success
+      console.log(response.data);
+      $scope.all_data=response.data;
+  },()=>{
+    //error
+    console.log(error);
+  });
+  //get country all_data
+  $scope.get_c_data=()=>{
+    let country=$scope.c;
+    if(country=="")
+    {
+      $scope.c_data=undefined;
+      return;
+    }
+    //use back tick"`" in giving url
+    $http.get(`${URL}/countries/${country}`).then((response)=>{
+      console.log(response.data);
+      $scope.c_data=response.data;
+    },(error)=>{
+      console.log(error);
+    });
+    //console.log($scope.c);
+  }
+});
